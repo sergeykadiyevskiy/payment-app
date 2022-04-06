8162599a6fca37c1620bb67412b6f298d1378a98
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-04-05 20:00:48 -0400

    stripe payment added 500 err still

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Pay from './comps/Pay';
+
+function App() {
+  return (
+    <Pay />
+  );
+}
+
+export default App;
---
+++ src/comps/Pay.jsx
@@ -0,0 +1,72 @@
+import StripeCheckout from "react-stripe-checkout";
+import { useState, useEffect } from "react";
+const axios = require('axios')
+
+const KEY =
+  "pk_test_51KlKF3Lox0HPXgKW3hmM7CaoMLH4Ooe2a2h5v2rYZuVQ2Do2RITshrzflOFnlmbqAI5LakWQBgrJWlpro96uA1Ho00rNCW5t0O";
+
+const Pay = () => {
+  const [stripeToken, setStripeToken] = useState(null);
+
+  const onToken = (token) => {
+    setStripeToken(token);
+  };
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await axios.post(
+          "http://localhost:3001/api/checkout/payment",
+          {
+            tokenId: stripeToken.id,
+            amount: 28500,
+          }
+        );
+
+        console.log(res.data)
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    stripeToken && makeRequest()
+  }, [stripeToken]);
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <StripeCheckout
+        name="Styles By Serve"
+        image="https://avatars.githubusercontent.com/u/51814805?v=4"
+        billingAddress
+        shippingAddress
+        description=" Your total is $285"
+        amount={28500}
+        token={onToken}
+        stripeKey={KEY}
+      >
+        <button
+          style={{
+            border: "none",
+            width: 120,
+            borderRadius: 5,
+            padding: "20px",
+            backgroundColor: "black",
+            color: "white",
+            fontWeight: "600",
+            cursor: "pointer",
+          }}
+        >
+          Pay Now
+        </button>
+      </StripeCheckout>
+    </div>
+  );
+};
+
+export default Pay;
